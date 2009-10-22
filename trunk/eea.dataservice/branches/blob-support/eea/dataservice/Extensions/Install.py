667d7cd553f5f1587750fecfce9cd8ac5ee73550
ghicaale <ghicaale@24821446-e7ea-0310-81ac-b189f4507b0b>
2009-10-22 14:56:02 +0000

    FlashUpload uninstall. Remove install step of eea.mapsandgraphs on install.

--- trunk/eea.dataservice/branches/blob-support/eea/dataservice/Extensions/Install.py
+++ trunk/eea.dataservice/branches/blob-support/eea/dataservice/Extensions/Install.py
@@ -2,6 +2,22 @@ from Products.CMFCore.utils import getToolByName
 
 def install(portal):
     setup_tool = getToolByName(portal, 'portal_setup')
+
+    # Remove deprecated eea.mapsandgraphs import steps
+    ir = setup_tool.getImportStepRegistry()
+    try:
+        del ir._registered['eea.mapsandgraphs-install-vocabularies']
+    except KeyError:
+        pass
+
+    # Dataservice import steps
     setup_tool.setImportContext('profile-eea.dataservice:default')
     setup_tool.runAllImportSteps()
+
+    # Uninstall PloneFlashUpload if installed
+    product_name = 'PloneFlashUpload'
+    qi = getToolByName(portal, 'portal_quickinstaller')
+    if qi.isProductInstalled(product_name):
+        qi.uninstallProducts([product_name])
+
     return "Ran all import steps."
