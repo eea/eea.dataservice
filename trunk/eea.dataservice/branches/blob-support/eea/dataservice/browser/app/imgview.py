4aeae4f7775bb16225876c5a01b990db95b44753
ghicaale <ghicaale@24821446-e7ea-0310-81ac-b189f4507b0b>
2009-11-03 13:58:30 +0000

    Thumbnails of EEAFigures are now shown in the folder listing. #2777

--- trunk/eea.dataservice/branches/blob-support/eea/dataservice/browser/app/imgview.py
+++ trunk/eea.dataservice/branches/blob-support/eea/dataservice/browser/app/imgview.py
@@ -4,13 +4,55 @@ from Products.Five.browser import BrowserView
 from valentine.imagescales.browser.interfaces import IImageView
 from valentine.imagescales.browser import atfield, atfolder
 
-class ImageView(BrowserView):
+class ImageViewFigure(BrowserView):
     """ Get cover image from folder contents
     """
     implements(IImageView)
 
     def __init__(self, context, request):
-        super(ImageView, self).__init__(context, request)
+        super(ImageViewFigure, self).__init__(context, request)
+        eeafile = None
+        files = self.context.objectValues('EEAFigureFile')
+        for obj in files:
+            if obj.getCategory() == 'hard':
+                eeafile = obj
+                break
+        if not eeafile and len(files):
+                eeafile = files[0]
+
+        if eeafile:
+            children = [img for img in eeafile.objectValues('ImageFS')
+                        if img.getId().lower().endswith('.png')]
+            if children:
+                self.original = children[0]
+                self.img = atfield.ImageView(self.original, request)
+            else:
+                self.original = None
+                self.img = atfolder.ImageView(eeafile, request)
+        else:
+            self.original = None
+            self.img = atfolder.ImageView(context, request)
+
+    def display(self, scalename='thumb'):
+        if scalename == 'original':
+            return not not self.original
+        return self.img.display(scalename)
+
+    def __call__(self, scalename='thumb'):
+        if self.display(scalename):
+            if scalename == 'original':
+                return self.original
+            return self.img(scalename)
+        raise NotFound(self.request, scalename)
+
+
+class ImageViewFigureFile(BrowserView):
+    """ Get cover image from folder contents
+    """
+    implements(IImageView)
+
+    def __init__(self, context, request):
+        super(ImageViewFigureFile, self).__init__(context, request)
         children = [img for img in context.objectValues('ImageFS')
                     if img.getId().lower().endswith('.png')]
         if children:
