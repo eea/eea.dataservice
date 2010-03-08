c8e2273482abbb24e75327782b4280f010dd8395
voineali <voineali@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-03-08 10:44:47 +0000

    Tagged eea.dataservice 1.8.

---
+++ trunk/eea.dataservice/tags/1.8/eea/dataservice/templates/interfaces.py
@@ -0,0 +1,7 @@
+from zope.interface import Interface
+
+
+class IProvideTemplates(Interface):
+    """ All objects that should have the ability to use templates service
+        implemented in eea.dataservice
+    """
