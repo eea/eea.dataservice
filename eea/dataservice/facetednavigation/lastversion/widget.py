8fdaa00bc494a135210fdbb315dca1bf7eee6780
Alin Voinea <contact@avoinea.com>
2010-03-15 15:34:29 +0000

    #3141 Update facetednavigation widget to use adapters instead of hardcoded method after_query.

--- eea/dataservice/facetednavigation/lastversion/widget.py
+++ eea/dataservice/facetednavigation/lastversion/widget.py
@@ -1,13 +1,16 @@
 """ Version widget
 """
-from DateTime import DateTime
-
+from zope.interface import implements
 from zope.app.pagetemplate.viewpagetemplatefile import ViewPageTemplateFile
 from eea.facetednavigation.widgets.widget import Widget as AbstractWidget
+
+from interfaces import ILastVersionWidget
 
 class Widget(AbstractWidget):
     """ Widget
     """
+    implements(ILastVersionWidget)
+
     # Widget properties
     widget_type = 'lastversion'
     widget_label = 'Show last version'
@@ -18,32 +21,3 @@ class Widget(AbstractWidget):
 
     index = ViewPageTemplateFile('widget.pt')
     edit_schema = AbstractWidget.edit_schema
-
-    def after_query(self, brains, form):
-        """ Filter brains
-        """
-        last_versions = {}
-        brains = [brain for brain in brains]
-        for brain in brains:
-            ptype = getattr(brain, 'portal_type', None)
-            version_id = getattr(brain, 'getVersionId', '')
-            if version_id:
-            #if ptype == 'Data' and len(version_id):
-                effective_date = getattr(brain, 'EffectiveDate', DateTime(1970))
-                if len(version_id):
-                    if last_versions.has_key(version_id):
-                        current_date = getattr(last_versions[version_id], 'EffectiveDate', DateTime(1970))
-                        if current_date < effective_date:
-                            last_versions[version_id] = brain
-                    else:
-                        last_versions[version_id] = brain
-
-        versions = [k.data_record_id_ for k in last_versions.values()]
-        for brain in brains:
-            version_id = getattr(brain, 'getVersionId', '')
-            #if ptype == 'Data' and len(version_id):
-            if version_id:
-                if brain.data_record_id_ in versions:
-                    yield brain
-            else:
-                yield brain
