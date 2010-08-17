a6d56098c93ac13611ab884819548bb5db2339cc
Alexandru Ghica <alec.ghica@eaudeweb.ro>
2010-08-17 14:31:37 +0000

    Data to ExternalDataSpec. #3213

--- eea/dataservice/browser/app/migrate.py
+++ eea/dataservice/browser/app/migrate.py
@@ -1,4 +1,14 @@
+import logging
 from Products.Five import BrowserView
+from zope.component import getMultiAdapter
+from Products.CMFCore.utils import getToolByName
+from Products.EEAContentTypes.interfaces import IRelations
+from Products.statusmessages.interfaces import IStatusMessage
+
+# Logging
+logger = logging.getLogger('eea.indicators')
+info = logger.info
+info_exception = logger.exception
 
 EXTCONTAINER = '/www/SITE/data-and-maps/data/external'
 
@@ -10,25 +20,102 @@ class MigrateDatasetsToExternalDatasets(BrowserView):
         eds_id = self.context.getId()
         extcontainer = self.context.unrestrictedTraverse(EXTCONTAINER)
         data_ob = self.context
+        request = self.request
+        msg = ''
+        err_msg = ''
 
         # Create new ExternalDataSpec object
-        eds_id = extcontainer.invokeFactory('ExternalDataSpec', id=eds_id)
-        eds_ob = getattr(extcontainer, eds_id)
+        try:
+            eds_id = extcontainer.invokeFactory('ExternalDataSpec', id=eds_id)
+            eds_ob = getattr(extcontainer, eds_id)
 
-        # Mapping metadata
-        eds_ob.setTitle(data_ob.Title())
-        eds_ob.setDescription(data_ob.Description())
-        eds_ob.setEffectiveDate(data_ob.getEffectiveDate())
-        eds_ob.setCreationDate(data_ob.creation_date)
-        eds_ob.setSubject(data_ob.Subject())
-        eds_ob.setCreators(data_ob.Creators())
+            # Mapping metadata
+            eds_ob.setTitle(data_ob.Title())
+            eds_ob.setDescription(data_ob.Description())
+            eds_ob.setEffectiveDate(data_ob.getEffectiveDate())
+            eds_ob.setCreationDate(data_ob.creation_date)
+            eds_ob.setSubject(data_ob.Subject())
+            eds_ob.setCreators(data_ob.Creators())
+
+            #TODO: * shall we map "dataSource" from the original Data object?
+            #      * shall we migrate/map themes ?
+            #      * shall we map something to provider_name, dataset_path
+            #        and category_of_use ?
+
+            processor = data_ob.getProcessor()
+            if processor:
+                eds_ob.setProvider_url(processor[0])
+                if len(processor) > 1:
+                    msg += 'Warning, there were more then one processor.'
+
+            owner = data_ob.getDataOwner()
+            if owner:
+                eds_ob.setDataset_url(owner[0])
+                if len(owner) > 1:
+                    msg += 'Warning, there were more then one owner.'
+
+            temporal = data_ob.getTemporalCoverage()
+            if temporal:
+                FormatTempCoverage = getMultiAdapter((data_ob, request),
+                                                      name=u'formatTempCoverage')
+                eds_ob.setTimeliness(FormatTempCoverage())
+
+            eds_ob.setOther_comments(data_ob.getMoreInfo())
+
+            # Map relatedItems. All forward and backward references of the
+            # data object are removed
+            forwards = data_ob.getRelatedItems()
+            backs = IRelations(data_ob).backReferences()
+            if forwards:
+                eds_ob.setRelatedItems(forwards)
+                data_ob.setRelatedItems([])
+            if backs:
+                for ob in backs:
+                    related = ob.getRelatedItems()
+                    del related[related.index(data_ob)]
+                    related.append(eds_ob)
+                    ob.setRelatedItems(related)
+
+            forwards = data_ob.getRelatedProducts()
+            backs = IRelations(data_ob).backReferences(relatesTo='relatesToProducts')
+            if forwards:
+                eds_ob.setRelatedItems(forwards)
+                data_ob.setRelatedProducts([])
+            if backs:
+                for ob in backs:
+                    related = ob.getRelatedProducts()
+                    del related[related.index(data_ob)]
+                    related.append(eds_ob)
+                    ob.setRelatedProducts(related)
+
+            # Change data object state to 'draft'
+            wftool = getToolByName(data_ob, 'portal_workflow')
+            state = wftool.getInfoFor(data_ob, 'review_state')
+            wf_msg = 'Set by migrate Data to ExternalDataSpec action.'
+
+            if state in ['published', 'published_eionet', 'visible']:
+                wftool.doActionFor(data_ob, 'retract', comment=wf_msg)
+                wftool.doActionFor(data_ob, 'enable', comment=wf_msg)
+            elif state in ['retracted']:
+                wftool.doActionFor(data_ob, 'enable', comment=wf_msg)
+            elif state in ['content_pending']:
+                wftool.doActionFor(data_ob, 'reject', comment=wf_msg)
+            elif state in ['draft']:
+                pass
+            else:
+                info('WARNING: Unknown state')
+            msg += 'Original data object was set to state: draft.'
+
+            eds_ob.reindexObject()
+        except Exception, err:
+            info('ERROR: on migrating Data to ExternalDataSpec')
+            info_exception('Exception: %s ', err)
+            err_msg = 'Error creating ExternalDataSpec, please check zope log.'
 
 
-        #TODO: Data -> ExternalDataSpec mapping
-        #TODO: relatedItems
-
-        eds_ob.reindexObject()
-
-        #TODO: delete or change state to old Data object?
-        #TODO: fix redirect
-        return 'Migration done.'
+        if err_msg:
+            IStatusMessage(request).addStatusMessage(err_msg, type='info')
+            request.response.redirect(data_ob.absolute_url())
+        else:
+            IStatusMessage(request).addStatusMessage(msg, type='info')
+            request.response.redirect(eds_ob.absolute_url())
