756b537298fe03d3a5c81f9e3d0cef4d1b9763e8
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-08-31 09:57:31 +0000

    Reindex Assessments and IndicatorFactSheets when changing state for an EEAFigure; #3475

--- trunk/eea.dataservice/trunk/eea/dataservice/events.py
+++ trunk/eea.dataservice/trunk/eea/dataservice/events.py
@@ -1,8 +1,12 @@
 """ New events
 """
-from zope.interface import Attribute
+
+from Acquisition import aq_inner, aq_parent
+from Products.EEAContentTypes.interfaces import IRelations
 from zope.app.event.interfaces import IObjectEvent
+from zope.interface import Attribute
 from zope.interface import implements
+
 
 class IFileUploadedEvent(IObjectEvent):
     """ Objects file uploaded
@@ -17,3 +21,18 @@ class FileUploadedEvent(object):
     def __init__(self, context, value):
         self.object = context
         self.value = value
+
+
+def handle_eeafigure_state_change(figure, event):
+    """Handler for EEAFigure workflow state change"""
+
+    #reindex all Assessments and IndicatorFactSheets that point to this figure
+    
+    backreferences = IRelations(figure).backReferences()
+    assessments = [aq_parent(aq_inner(a)) for a in backreferences 
+                                          if a.meta_type == "AssessmentPart"]
+    ifs = filter(lambda o:o.meta_type=="IndicatorFactSheet", backreferences)
+
+    for obj in assessments + ifs:
+        obj.reindexObject()
+
