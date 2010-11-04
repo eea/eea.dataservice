9ddcc1d7518eba1a53037dbcf54ae89700a21aee
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-11-04 14:28:34 +0000

    Make starting year 1750 instead of 1850

--- trunk/eea.dataservice/trunk/eea/dataservice/config.py
+++ trunk/eea.dataservice/trunk/eea/dataservice/config.py
@@ -10,7 +10,7 @@ DEFAULT_ADD_CONTENT_PERMISSION = "Add portal content"
 COPYRIGHTS = u'(c) Copyrights - EEA (European Environment Agency)'
 
 #Dataset years vocabulary
-STARTING_YEAR = 1850
+STARTING_YEAR = 1750
 
 #Obligations vocabulary
 ROD_SERVER = 'http://rod.eionet.europa.eu/rpcrouter'
