b387899aef54cca18ae0a82418a76038748f37d6
Alexandru Ghica <alec.ghica@eaudeweb.ro>
2010-12-15 17:01:54 +0000

    Work in progress on importing soer files. refs #7971

--- eea/dataservice/updates/soer_bulk_import.py
+++ eea/dataservice/updates/soer_bulk_import.py
@@ -7,6 +7,7 @@ __author__ = """European Environment Agency (EEA)"""
 __docformat__ = 'plaintext'
 __credits__ = """contributions: Alec Ghica"""
 
+import transaction
 from Products.Five.browser import BrowserView
 from eea.dataservice.updates.soer_bulk_import_data import soer_data
 
@@ -16,6 +17,9 @@ logger = logging.getLogger('eea.dataservice')
 info = logger.info
 info_exception = logger.exception
 
+# Configuration
+IMPORT_PATH = "/www/SITE/sandbox/soer-figures"
+FILES_PATH = "/var/local/soer_files/FINAL_FIGURES"
 
 class BulkImportSoerFigures(BrowserView):
     """ Bulk import of SOER figures """
@@ -26,19 +30,59 @@ class BulkImportSoerFigures(BrowserView):
         1. create EEAFigures
         2. create EEAFigureFiles
         3. extra mappings
-        4. transactional
+        4. transactional import
+        5. check encoding during import, e.g. José Barredo
     3. generate import logs
     4. run a full test on unicorn (including files)
     """
 
     questions = """
-    1. on http://unicorn:3333/project.html?project=1984853699455 the first
-        7 rows must be deleted as are just my tooltip and example right?
+    1. what state should have imported figures and files?
     """
 
     def __call__(self):
-        try:
-            info('INFO: Done soer figures import!')
-        except Exception, err:
-            info('ERR: import error')
-            info_exception(err)
+        for row in soer_data['rows']:
+            object_type = row["Object type"]
+            filepath = row["Filepath"]
+            category = row["Category"]
+            title = row["Title"]
+            creators = row["Creators"]
+            contributors = row["Contributors"]
+            copyrights = row["Copyrights"]
+            keywords = row["Keywords"]
+            figure_type = row["Figure type"]
+            description = row["Description"]
+            themes = row["Themes"]
+            geo_coverage = row["Geographical coverage"]
+            eea_management_plan = row["EEA Management Plan"]
+            last_upload = row["Last upload"]
+            owner = row["Owner"]
+            processor = row["Processor"]
+            tem_coverage = row["Temporal coverage"]
+            contact = row["Contact person(s) for EEA"]
+            source = row["Source"]
+            additional_information = row["Additional information"]
+            methodology = row["Methodology"]
+            unit = row["Unit"]
+
+            try:
+                import pdb; pdb.set_trace()
+                if row['Object type'] == 'EEAFigure':
+                    pass
+                elif row['Object type'] == 'EEAFigureFile':
+                    pass
+                else:
+                    info('ERROR: unknown "Object type" on %s', filepath)
+            except Exception, err:
+                info('ERR: import error on %s', filepath)
+                info_exception(err)
+
+        transaction.commit()
+        info('INFO: Done soer figures import!')
+
+
+
+
+
+
+
