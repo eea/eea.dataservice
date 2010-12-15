d58533e25d55b59fd1299c323e8e259b2903ad03
Alexandru Ghica <alec.ghica@eaudeweb.ro>
2010-12-15 16:08:08 +0000

    Added dumped metadata. refs #7971

--- eea/dataservice/updates/soer_bulk_import.py
+++ eea/dataservice/updates/soer_bulk_import.py
@@ -8,6 +8,7 @@ __docformat__ = 'plaintext'
 __credits__ = """contributions: Alec Ghica"""
 
 from Products.Five.browser import BrowserView
+from eea.dataservice.updates.soer_bulk_import_data import soer_data
 
 # Logging
 import logging
@@ -18,6 +19,22 @@ info_exception = logger.exception
 
 class BulkImportSoerFigures(BrowserView):
     """ Bulk import of SOER figures """
+
+    import_steps = """
+    1. export metadata in XML
+    2. import from XML
+        1. create EEAFigures
+        2. create EEAFigureFiles
+        3. extra mappings
+        4. transactional
+    3. generate import logs
+    4. run a full test on unicorn (including files)
+    """
+
+    questions = """
+    1. on http://unicorn:3333/project.html?project=1984853699455 the first
+        7 rows must be deleted as are just my tooltip and example right?
+    """
 
     def __call__(self):
         try:
