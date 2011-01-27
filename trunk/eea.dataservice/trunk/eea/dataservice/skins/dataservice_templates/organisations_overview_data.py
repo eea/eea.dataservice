8fa426819868f86235364764ee296ead4651d844
ghicaale <ghicaale@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-01-27 10:13:25 +0000

    Added "organisations overview" portlet in order to simplify the management of duplicates and wrong metadata on organisations. Refs #7559

--- trunk/eea.dataservice/trunk/eea/dataservice/skins/dataservice_templates/organisations_overview_data.py
+++ trunk/eea.dataservice/trunk/eea/dataservice/skins/dataservice_templates/organisations_overview_data.py
@@ -5,7 +5,7 @@
 ##bind namespace=
 ##bind script=script
 ##bind subpath=traverse_subpath
-##parameters=
+##parameters=info
 
 from Products.CMFCore.utils import getToolByName
 
@@ -13,33 +13,47 @@ request = context.REQUEST
 response = request.RESPONSE
 
 cat = getToolByName(context, 'portal_catalog', None)
-
 query = {
 'show_inactive':True,
 'language':'ALL',
 'portal_type': 'Organisation',}
-
 brains = cat(**query)
 
-data = {}
-full_data = {}
-res = []
+if info == 'references':
+ res = []
+ data = {}
+ full_data = {}
 
-for k in brains:
- org_url = k.getUrl
- data[org_url] = 0
- full_data[org_url] = k
+ for k in brains:
+  org_url = k.getUrl
+  data[org_url] = 0
+  full_data[org_url] = k
 
- query = {'portal_type':['EEAFigure','Data'], 'getDataOwner': org_url}
- owner_brains = cat(**query)
- data[org_url] = data[org_url] + len(owner_brains)
+  query = {'portal_type':['EEAFigure','Data'], 'getDataOwner': org_url}
+  owner_brains = cat(**query)
+  data[org_url] = data[org_url] + len(owner_brains)
 
- query = {'portal_type':['EEAFigure','Data'], 'getProcessor': org_url}
- proc_brains = cat(**query)
- data[org_url] = data[org_url] + len(proc_brains)
+  query = {'portal_type':['EEAFigure','Data'], 'getProcessor': org_url}
+  proc_brains = cat(**query)
+  data[org_url] = data[org_url] + len(proc_brains)
 
-for k in data.keys():
- if data[k] == 0:
-  res.append(full_data[k])
+ for k in data.keys():
+  if data[k] == 0:
+   res.append(full_data[k])
+elif info == 'duplicates':
+ urls = []
+ res = {}
+ for k in brains:
+  org_url = k.getUrl
+  urls.append(org_url)
+  if org_url.endswith('/'):
+   org_url = org_url[:len(org_url)-1]
+
+  if res.has_key(org_url):
+   res[org_url].append(k)
+  else:
+   res[org_url] = [k]
+else:
+ res = None
 
 return res
