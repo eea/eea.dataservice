738d0afbabeb66ab7bcdb9a7c3b66edb0e08b793
demarant <demarant@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-03-07 11:13:23 +0000

    make code required and enable validator for figures refs #8158

--- trunk/eea.dataservice/trunk/eea/dataservice/content/EEAFigure.py
+++ trunk/eea.dataservice/trunk/eea/dataservice/content/EEAFigure.py
@@ -74,7 +74,6 @@ schema = Schema((
 ),)
 
 eeafigure_schema = dataservice_schema.copy() + schema.copy()
-eeafigure_schema['eeaManagementPlan'].validators = ()
 
 # Set position on form
 eeafigure_schema.moveField('figureType', pos=3)
