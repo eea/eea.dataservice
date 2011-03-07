e384b0652c77f36f4d9fd068029c0063637a66ce
Alexandru Ghica <alec.ghica@eaudeweb.ro>
2011-03-07 16:26:36 +0000

    Moved "management_plan_code_validator" validator under Products.EEAContentTypes package as was used before being registered. Refs #8158

--- eea/dataservice/content/schema.py
+++ eea/dataservice/content/schema.py
@@ -24,33 +24,6 @@ from eea.dataservice.vocabulary import (
 # Validators
 from Products.validation.interfaces.IValidator import IValidator
 from Products.validation import validation
-class ManagementPlanCodeValidator:
-    __implements__ = IValidator
-
-    def __init__(self,
-                 name,
-                 title='Management plan code',
-                 description='Management plan code validator'):
-        self.name = name
-        self.title = title or name
-        self.description = description
-
-    def __call__(self, value, *args, **kwargs):
-        """ check if management plan code is in right format """
-        # check we have 3 digits separated by dots
-        errmsg = "Validation failed, management plan code is not in the format #.#.# e.g. 1.5.2"
-        digits = value[1].split('.')
-        if len(digits) == 3:
-            for dig in digits:
-                try:
-                    nval = int(dig)
-                except ValueError:
-                    return (errmsg)
-            return 1
-        else:
-            return (errmsg)
-
-validation.register(ManagementPlanCodeValidator('management_plan_code_validator'))
 
 class UniqueOrganisationUrlValidator:
     __implements__ = IValidator
