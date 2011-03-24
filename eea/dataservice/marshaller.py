30d2446a70470526616b8a9f13ca1bc26a4ccc7c
tiberich <tiberiuichim@users.noreply.github.com>
2011-03-24 15:14:44 +0000

    Refs #8103; Added rdfmarshaller implementation for management plan field

--- eea/dataservice/marshaller.py
+++ eea/dataservice/marshaller.py
@@ -3,14 +3,20 @@
 
 from Products.Archetypes.interfaces import IField
 from eea.rdfmarshaller.interfaces import IArchetype2Surf, ISurfSession
-from eea.rdfmarshaller.marshaller import ATField2RdfSchema
+from eea.rdfmarshaller.marshaller import ATField2RdfSchema, ATField2Surf
 from zope.component import adapts
 from zope.interface import implements, Interface
+from eea.dataservice.fields.ManagementPlanField import ManagementPlanField
 
 
-class ManagementField2RdfSchema(ATField2RdfSchema):
-    """IArchetype2Surf implemention for Fields"""
+class ManagementPlanField2Surf(ATField2Surf):
+    """Base implementation of IATField2Surf"""
+    adapts(ManagementPlanField, ISurfSession)
 
-    implements(IArchetype2Surf)
-    adapts(IField, Interface, ISurfSession)
+    def value(self, context):
+        v = self.field.getAccessor(context)()
+        if v and (len(v) == 2):
+            return "Year: %s, code: %s" % (v[0], v[1])
+        return " - ".join(v)
+
 
