d38699664dd7fab1e122153f42fe2cc0919343f6
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-03-25 10:31:32 +0000

    Tagged eea.dataservice 1.33.

---
+++ trunk/eea.dataservice/tags/1.33/eea/dataservice/fields/__init__.py
@@ -0,0 +1,4 @@
+from eea.dataservice.fields import ManagementPlanField
+ManagementPlanField # pyflakes
+from eea.dataservice.fields.FileField import EventFileField
+EventFileField # pyflakes
---
+++ trunk/eea.dataservice/tags/1.33/eea/dataservice/marshaller.py
@@ -0,0 +1,22 @@
+""" eea.rdfmarshaller customizations for eea.dataservice
+"""
+
+from Products.Archetypes.interfaces import IField
+from eea.rdfmarshaller.interfaces import IArchetype2Surf, ISurfSession
+from eea.rdfmarshaller.marshaller import ATField2RdfSchema, ATField2Surf
+from zope.component import adapts
+from zope.interface import implements, Interface
+from eea.dataservice.fields.ManagementPlanField import ManagementPlanField
+
+
+class ManagementPlanField2Surf(ATField2Surf):
+    """Base implementation of IATField2Surf"""
+    adapts(ManagementPlanField, ISurfSession)
+
+    def value(self, context):
+        v = self.field.getAccessor(context)()
+        if v and (len(v) == 2):
+            return "%s %s" % (v[0], v[1])
+        return " - ".join(v)
+
+
