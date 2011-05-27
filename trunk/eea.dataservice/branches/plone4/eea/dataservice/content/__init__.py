af477c31f4d5922b0f8cd7a708201ce720be138e
voineali <voineali@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-05-27 13:43:54 +0000

    Refs #8287  - Started plone4 branch.

---
+++ trunk/eea.dataservice/branches/plone4/eea/dataservice/content/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+__author__ = """European Environment Agency (EEA)"""
+__docformat__ = 'plaintext'
+
+
+from eea.dataservice.content import Data
+from eea.dataservice.content import Organisation
+from eea.dataservice.content import DataFile
+from eea.dataservice.content import DataTable
+from eea.dataservice.content import EEAFigure
+from eea.dataservice.content import EEAFigureFile
+from eea.dataservice.content import ImageFS
+
+Data, Organisation, DataFile, DataTable, EEAFigure, EEAFigureFile, ImageFS #pylint pyflakes
