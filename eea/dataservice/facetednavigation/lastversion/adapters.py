7208cdb9594195d9ddcd8d809d09e3dff59bf9b0
Alexandru Ghica <alec.ghica@eaudeweb.ro>
2012-02-02 18:35:50 +0000

    * WIP -> Feature: Under organisations view also count external data spec relations    refs #8644    refs #7458]  * WIP -> Feature: Count only last version under organisation view relations    refs #8470

--- eea/dataservice/facetednavigation/lastversion/adapters.py
+++ eea/dataservice/facetednavigation/lastversion/adapters.py
@@ -18,10 +18,8 @@ class WidgetFilterBrains(object):
         last_versions = {}
         brains = [brain for brain in brains]
         for brain in brains:
-            #ptype = getattr(brain, 'portal_type', None)
             version_id = getattr(brain, 'getVersionId', '')
             if version_id:
-            #if ptype == 'Data' and len(version_id):
                 effective_date = getattr(brain, 'EffectiveDate', DateTime(1970))
                 if len(version_id):
                     if last_versions.has_key(version_id):
@@ -36,7 +34,6 @@ class WidgetFilterBrains(object):
         versions = [k.data_record_id_ for k in last_versions.values()]
         for brain in brains:
             version_id = getattr(brain, 'getVersionId', '')
-            #if ptype == 'Data' and len(version_id):
             if version_id:
                 if brain.data_record_id_ in versions:
                     yield brain
