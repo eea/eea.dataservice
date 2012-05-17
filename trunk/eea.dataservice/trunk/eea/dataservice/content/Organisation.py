8afbf4d2a5f257954ab4b16b4e846d01bcd5836c
ichimdav <ichimdav@24821446-e7ea-0310-81ac-b189f4507b0b>
2012-05-17 12:36:28 +0000

    Refs #8773; use geotags widget instead of locationwiget for the location field of the organisation contenttype

--- trunk/eea.dataservice/trunk/eea/dataservice/content/Organisation.py
+++ trunk/eea.dataservice/trunk/eea/dataservice/content/Organisation.py
@@ -1,17 +1,17 @@
 """ Organisation
 """
 from zope.interface import implements
-from Products.Archetypes.atapi import Schema, StringWidget, StringField
+from Products.Archetypes.atapi import Schema, StringWidget
 from Products.Archetypes.atapi import TextField, TextAreaWidget
 from Products.CMFCore import permissions
 from AccessControl import ClassSecurityInfo
 from Products.ATContentTypes.content.folder import ATFolder
 from Products.ATContentTypes.content.folder import ATFolderSchema
-from Products.CMFCore.permissions import ModifyPortalContent
 
 from eea.dataservice.interfaces import IOrganisation
 from eea.dataservice.fields.OrganisationField import OrganisationField
-from eea.locationwidget.locationwidget import LocationWidget
+from eea.geotags.field.location import GeotagsLinesField
+from eea.geotags.widget.location import GeotagsWidget
 
 schema = Schema((
     OrganisationField(
@@ -29,15 +29,13 @@ schema = Schema((
             i18n_domain='eea',
         )
     ),
-    StringField(
-        name='location',
-        searchable=True,
-        accessor='getLocation',
-        mutator='setLocation',
-        widget=LocationWidget(
-            description=("Use the address to retrieve the location <em>"
-                         "(e.g. Kongens Nytorv 6, 1050 Copenhagen K, Denmark)"
-                         "</em>"),
+
+    GeotagsLinesField('location',
+        schemata='default',
+        widget= GeotagsWidget(
+            description='Geographical location '
+                             'related to this content. Click Edit button '
+                             'to select a location',
             description_msgid="dataservice_help_address",
             label="Organisation address",
             label_msgid="dataservice_label_address",
@@ -86,15 +84,3 @@ class Organisation(ATFolder):
         field = self.getField('organisationUrl')
         return field.getAccessor(self)()
 
-    security.declareProtected(ModifyPortalContent, 'setLocation')
-    def setLocation(self, value, **kw):
-        """ Location mutator. """
-        value = value.replace('<street address>, <city>, <country>', '')
-        self.getField('location').set(self, value)
-
-    security.declareProtected(permissions.View, 'getLocation')
-    def getLocation(self):
-        """ Location accessor. """
-        value = self.getField('location').get(self)
-        value = value.replace('<street address>, <city>, <country>', '')
-        return value
