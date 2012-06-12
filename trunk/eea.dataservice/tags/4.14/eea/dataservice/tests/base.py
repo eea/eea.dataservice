48df39681bb592b94f2c0a868ce96bc57609acac
demarant <demarant@24821446-e7ea-0310-81ac-b189f4507b0b>
2012-06-12 13:55:36 +0000

    Tagged eea.dataservice 4.14.

---
+++ trunk/eea.dataservice/tags/4.14/eea/dataservice/tests/base.py
@@ -0,0 +1,35 @@
+""" Base test cases
+"""
+from plone.testing import z2
+from plone.app.testing import FunctionalTesting
+from plone.app.testing import PloneSandboxLayer
+from plone.app.testing import applyProfile
+
+class EEAFixture(PloneSandboxLayer):
+    """ Custom fixture
+    """
+    def setUpZope(self, app, configurationContext):
+        """ Setup Zope
+        """
+        import eea.dataservice
+        import eea.workflow
+        self.loadZCML(package=eea.dataservice)
+        self.loadZCML(package=eea.workflow)
+
+        z2.installProduct(app, 'Products.ATVocabularyManager')
+        z2.installProduct(app, 'eea.dataservice')
+
+    def tearDownZope(self, app):
+        """ Uninstall Zope
+        """
+        z2.uninstallProduct(app, 'Products.ATVocabularyManager')
+        z2.uninstallProduct(app, 'eea.dataservice')
+
+    def setUpPloneSite(self, portal):
+        """ Setup Plone
+        """
+        applyProfile(portal, 'eea.dataservice:default')
+
+EEAFIXTURE = EEAFixture()
+FUNCTIONAL_TESTING = FunctionalTesting(bases=(EEAFIXTURE,),
+                                       name='EEADataservice:Functional')
