98bd323d1ab8ea296b04e1ffb1b236e3643eb263
ichimdav <ichimdav@24821446-e7ea-0310-81ac-b189f4507b0b>
2012-11-13 16:04:26 +0000

    Refs #9423; replace geotags information with the dynamic entry only when clicking on save

--- trunk/eea.dataservice/trunk/eea/dataservice/skins/dataservice_templates/countries_widget.js
+++ trunk/eea.dataservice/trunk/eea/dataservice/skins/dataservice_templates/countries_widget.js
@@ -84,30 +84,35 @@ function setWidgetSync() {
 // add Dynamic Geotags checkbox on geotags widget in Data and EEAFigure edit form
 // TODO: #9423 move this code in a geotags related file
 function setDynamicGeotags() {
-  jQuery("<p><input type='checkbox' id='dynamic_geotags' />" +
-         "<span id='dynamic_geotags_span'>Dynamic Geotags Coverage</span></p>")
+    jQuery("<p><input type='checkbox' id='dynamic_geotags' />" +
+            "<span id='dynamic_geotags_span'>Dynamic Geotags Coverage</span></p>")
         .insertAfter('#location_help');
-  jQuery('<span class="formHelp" id="dynamic_geotags_help">' +
-         'Warning: Checking this option will remove any previously set geotags</span>')
+    jQuery('<span class="formHelp" id="dynamic_geotags_help">' +
+            'Warning: Saving the document with this option will remove any previously set geotags</span>')
         .insertAfter("#dynamic_geotags_span");
-  var location_edit = jQuery("#location-edit");
-  jQuery("#dynamic_geotags").click(function() {
-      if(jQuery(this).is(":checked") ) {
-          location_edit.attr('disabled', true);
-          jQuery("#location-geopreview").prev().text(
-            '{"type": "FeatureCollection", "features": [{"geometry":' +
-            '{"type": "Point", "coordinates": [0, 0]}, "type": "Feature",' + 
-            '"bbox": [], "properties": {"description": "", "tags": "area",' +
-            '"country": null, "center": [0, 0], "other": {"name": "Dynamic",' +
-            '"geonameId": 6295630, "toponymName": "Dynamic",' +
-            '"fclName": "parks,area, ...", "fcode": "AREA", "lat": 0, "lng": 0,' +
-            '"fcl": "L", "fcodeName": "area"}, "title": "Dynamic", "name": "6295630"}}]}');
-      }
-      else { 
-          location_edit.attr('disabled', false);
-      }
-  });
+    var location_edit = jQuery("#location-edit"),
+        dynamic_geotags = jQuery("#dynamic_geotags");
+    dynamic_geotags.click(function() {
+        if(jQuery(this).is(":checked")) {
+            location_edit.attr('disabled', true);
+        }
+        else { 
+            location_edit.attr('disabled', false);
+        }
+    });
 
+    jQuery("input[value='Save']").click(function() {
+        if(dynamic_geotags.is(':checked')) {
+            jQuery('#location-geopreview').prev().text(
+                '{"type": "FeatureCollection", "features": [{"geometry":' +
+                    '{"type": "Point", "coordinates": [0, 0]}, "type": "Feature",' + 
+                '"bbox": [], "properties": {"description": "", "tags": "area",' +
+                    '"country": null, "center": [0, 0], "other": {"name": "Dynamic",' +
+                        '"geonameId": 6295630, "toponymName": "Dynamic",' +
+                '"fclName": "parks,area, ...", "fcode": "AREA", "lat": 0, "lng": 0,' +
+                '"fcl": "L", "fcodeName": "area"}, "title": "Dynamic", "name": "6295630"}}]}');
+        }
+    });
 }
 jQuery(document).ready(function() {
   setWidgetSync();
