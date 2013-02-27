4f7181f578055a762cfb0b6214dcb9871528f486
ichimdav <ichimdav@24821446-e7ea-0310-81ac-b189f4507b0b>
2013-02-27 16:19:03 +0000

    Tagged eea.dataservice 6.4.

---
+++ trunk/eea.dataservice/tags/6.4/eea/dataservice/facetednavigation/geocoverage/__init__.py
@@ -0,0 +1,2 @@
+""" Geocoverage widget for eea.facetednavigation
+"""
---
+++ trunk/eea.dataservice/tags/6.4/eea/dataservice/skins/dataservice_templates/dataset_view.js
@@ -0,0 +1,23 @@
+/* Show/Hide table definition */
+function showDefinition(context) {
+    if (jQuery(context).html() === '[+]') {
+        jQuery(context).parent().next().css("display","block");
+        jQuery(context).html('[x]');
+    }
+    else {
+        jQuery(context).parent().next().css("display","none");
+        jQuery(context).html('[+]');
+    }
+}
+
+function setTableDefShow() {
+    jQuery('.table-definition-show').click(function () { showDefinition(this); });
+}
+
+jQuery(document).ready(function() {
+    setTableDefShow();
+    if (window.DataService) {
+        window.DataService.Load();
+    }
+});
+
