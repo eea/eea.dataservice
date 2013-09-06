5ca800136df627cd0ff08f80c6629ab27e46f26a
ichimdav <ichimdav@24821446-e7ea-0310-81ac-b189f4507b0b>
2013-09-06 15:08:30 +0000

    Tagged eea.dataservice 7.3.

---
+++ trunk/eea.dataservice/tags/7.3/eea/dataservice/config.py
@@ -0,0 +1,15 @@
+""" Package config
+"""
+
+PROJECTNAME = "eea.dataservice"
+product_globals = globals()
+
+AUTHOR = u"European Environment Agency"
+DEFAULT_ADD_CONTENT_PERMISSION = "Add portal content"
+COPYRIGHTS = u'(c) Copyrights - EEA (European Environment Agency)'
+
+#Obligations vocabulary
+ROD_SERVER = 'http://rod.eionet.europa.eu/rpcrouter'
+
+#
+FILE_FIELDS = ['file', 'image', 'blob']
