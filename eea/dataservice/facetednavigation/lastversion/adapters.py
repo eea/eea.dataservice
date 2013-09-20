71cc157724c9fbe1aa6c96910decb7fe6d7d926b
Alexandru Ghica <alec.ghica@eaudeweb.ro>
2013-09-20 16:05:12 +0000

    * Bug fix: fixed sorting for show last version facetnav widget   [ghicaale refs #16784]

--- eea/dataservice/facetednavigation/lastversion/adapters.py
+++ eea/dataservice/facetednavigation/lastversion/adapters.py
@@ -3,6 +3,7 @@
 from zope.interface import implements
 from eea.facetednavigation.interfaces import IWidgetFilterBrains
 from DateTime import DateTime
+import operator
 
 class WidgetFilterBrains(object):
     """ Filter brains after query
@@ -18,7 +19,7 @@ class WidgetFilterBrains(object):
         last_versions = {}
         seventies = DateTime(1970)
 
-        for brain in brains:
+        for i, brain in enumerate(brains):
             version_id = getattr(brain, 'getVersionId', '')
             effective_date = (
                 getattr(brain, 'EffectiveDate', None) or
@@ -28,15 +29,16 @@ class WidgetFilterBrains(object):
 
             if last_versions.has_key(version_id):
                 current_date = (
-                    getattr(last_versions[version_id],
+                    getattr(last_versions[version_id][0],
                             'EffectiveDate', None) or
-                    getattr(last_versions[version_id],
+                    getattr(last_versions[version_id][0],
                             'CreationDate', None) or
                     seventies
                 )
                 if current_date < effective_date:
-                    last_versions[version_id] = brain
+                    last_versions[version_id] = (brain, i)
             else:
-                last_versions[version_id] = brain
+                last_versions[version_id] = (brain, i)
 
-        return last_versions.values()
+        return [res[0] for res in
+                   sorted(last_versions.values(), key=operator.itemgetter(1))]
