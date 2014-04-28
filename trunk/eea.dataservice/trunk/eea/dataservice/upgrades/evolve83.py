74142d80d9b1195fe286032eeeb7ad8132b8beb4
ichimdav <ichimdav@24821446-e7ea-0310-81ac-b189f4507b0b>
2014-04-28 15:14:11 +0000

    Refs #19367; added upgrade step for the given changes

---
+++ trunk/eea.dataservice/trunk/eea/dataservice/upgrades/evolve83.py
@@ -0,0 +1,37 @@
+"""  Upgrade steps to version 8.3
+"""
+
+import logging
+from Products.CMFCore.utils import getToolByName
+import transaction
+
+logger = logging.getLogger('eea.soercontent')
+
+
+def reindex_geo_coverage(context):
+    """ Migrate to folderish
+    """
+    ctool = getToolByName(context, 'portal_catalog')
+    brains = ctool.unrestrictedSearchResults(
+        portal_type=['Data', 'EEAFigure'])
+
+    total = len(brains)
+    logger.info('Reindexing %s Data and EEAFigures...', total)
+    count = 0
+    for brain in brains:
+        logger.info('Reindexing %s', brain.getURL())
+        try:
+            doc = brain.getObject()
+            doc.reindexObject(idxs=['getGeographicCoverage'])
+            count += 1
+            if count % 100 == 0:
+                logger.info('INFO: Transaction committed to zodb (%s/%s)',
+                         count, total)
+                transaction.commit()
+        except Exception, err:
+            logger.warn('Couldn\'t migrate %s', brain.getURL())
+            logger.exception(err)
+            continue
+
+    logger.info('Reindexing Data and EEAFigures... DONE')
+    return 'Done migrating %s' % total
