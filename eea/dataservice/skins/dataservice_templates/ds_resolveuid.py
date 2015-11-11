374b5d6ae582e7ccf169465d469791b2c03b7dc0
Luca Pisani <luca.pisani@abstract.it>
2015-11-11 16:24:39 +0000

    refs #30065 edit ds_resolveuid to call via python code

--- eea/dataservice/skins/dataservice_templates/ds_resolveuid.py
+++ eea/dataservice/skins/dataservice_templates/ds_resolveuid.py
@@ -5,7 +5,7 @@
 ##bind namespace=
 ##bind script=script
 ##bind subpath=traverse_subpath
-##parameters=
+##parameters=uuid='',redirect=True
 # (reference_url is supposed to do the same thing, but is broken)
 from Products.CMFCore.utils import getToolByName
 from Products.PythonScripts.standard import html_quote
@@ -14,10 +14,11 @@ from AccessControl import Unauthorized
 request = context.REQUEST
 response = request.RESPONSE
 
-try:
-    uuid = traverse_subpath.pop(0)
-except:
-    raise Unauthorized, context
+if not uuid:
+    try:
+        uuid = traverse_subpath.pop(0)
+    except:
+        raise Unauthorized, context
 
 reference_tool = getToolByName(context, 'reference_catalog')
 obj = reference_tool.lookupObject(uuid)
@@ -34,6 +35,9 @@ if not obj:
         res = resView(query)
         if len(res) > 0:
             redirect_to = context.absolute_url() + '/' + res[len(res)-1].getId
+            if not redirect:
+                # return find url
+                return redirect_to
             return response.redirect(redirect_to, lock=1)
 
         # Redirects for short IDs
@@ -48,11 +52,17 @@ if not obj:
             query = {'portal_type': ptype,
                      'show_inactive': True,
                      'getShortId': request.get('id', None)}
-            resView = context.restrictedTraverse('@@getDataForRedirect')
             res = resView(query)
             if len(res) > 0:
                 redirect_to = context.absolute_url() + '/' + res[0].getId
-                return context.REQUEST.RESPONSE.redirect(redirect_to, lock=1)
+                if not redirect:
+                    # return find url
+                    return redirect_to
+                return response.redirect(redirect_to, lock=1)
+
+        if not redirect:
+            # return None if not found
+            return None
 
         # Redirects for Not Found
         return response.notFoundError('''The link you followed appears to be broken''')
@@ -65,4 +75,9 @@ else:
 
 if request.QUERY_STRING:
     target += '?' + request.QUERY_STRING
+
+if not redirect:
+    # return find url
+    return target
+
 return response.redirect(target, status=301)
