81b86ac22d0ff17201a51d506d21bb5f6f4f80a1
Alin Voinea <alin.voinea@eaudeweb.ro>
2018-12-03 15:59:37 +0200

    Refs #100588 - Fixed ROD server URL

--- eea/dataservice/config.py
+++ eea/dataservice/config.py
@@ -9,7 +9,7 @@ DEFAULT_ADD_CONTENT_PERMISSION = "Add portal content"
 COPYRIGHTS = u'(c) Copyrights - EEA (European Environment Agency)'
 
 # Obligations vocabulary
-ROD_SERVER = 'http://rod.eionet.europa.eu/rpcrouter'
+ROD_SERVER = 'https://rod.eionet.europa.eu/rpcrouter'
 SOCKET_TIMEOUT = 5.0  # in seconds
 
 #
