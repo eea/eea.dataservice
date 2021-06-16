0641140836363a83c7744ce8d0cbe313b749c87a
David Ichim <ichim.david@gmail.com>
2021-06-16 21:07:42 +0300

    Refs #130851 get provenance data from external data spec relation

--- eea/dataservice/data/source.py
+++ eea/dataservice/data/source.py
@@ -1,43 +1,30 @@
 """ figure multi data provenance
 """
 from zope.interface import implements
-from zope.component import queryAdapter
 
 from eea.app.visualization.interfaces import IMultiDataProvenance
 from eea.app.visualization.data.source import MultiDataProvenance
 
 
 class EEAFigureMultiDataProvenance(MultiDataProvenance):
-    """ eea figure multi data provenance
+    """ eea figure multi data provenance from relations
     """
     implements(IMultiDataProvenance)
 
     def defaultProvenances(self):
         """ default provenances
         """
-        relatedProvenances = ()
+
+        relatedProvenances = []
         relatedItems = self.context.getRelatedItems()
-        orderindex = 0
         for item in relatedItems:
             if item.portal_type != 'ExternalDataSpec':
                 continue
-            source = queryAdapter(item, IMultiDataProvenance)
+            related_dict = {
+                'title': getattr(item, 'title', ''),
+                'owner': getattr(item, 'provider_url', ''),
+                'link': getattr(item, 'dataset_url', ''),
+            }
+            relatedProvenances.append(related_dict)
 
-            is_provenance_info = False
-            for provenance in source.provenances:
-                if provenance['link'] == self.context.absolute_url():
-                    is_provenance_info = True
-            if is_provenance_info:
-                continue
-
-            item_provenances = getattr(source, 'provenances')
-            for item_provenance in item_provenances:
-                dict_item_provenance = dict(item_provenance)
-                if dict_item_provenance.get('title', '') != '' and \
-                    dict_item_provenance.get('link', '') != '' and \
-                    dict_item_provenance.get('owner', '') != '':
-                    dict_item_provenance['orderindex_'] = orderindex
-                    orderindex = orderindex + 1
-                    relatedProvenances = relatedProvenances + \
-                                        (dict_item_provenance,)
         return relatedProvenances
